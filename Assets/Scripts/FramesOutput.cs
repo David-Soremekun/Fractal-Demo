fc827478abd357bc2f9e83d73611ea4da8ee0eca
David S <124046476+David-Soremekun@users.noreply.github.com>
2023-02-22 12:03:50 +0000

    init commit

--- Assets/Scripts/FramesOutput.cs
+++ Assets/Scripts/FramesOutput.cs
@@ -7,13 +7,33 @@ using UnityEngine.UI;
 
 public class FramesOutput : MonoBehaviour
 {
-    public Text fpsText;
-    public float deltaTime;
+    public int m_frameCounter;
+    private float m_timeCounter;
+    public float m_lastFramerate;
+    public float m_refreshTime;
+
+
+    void Start()
+    {
+        m_frameCounter = 0;
+        m_timeCounter = 0.0f;
+        m_lastFramerate = 0.0f;
+        m_refreshTime = 0.5f;
+}
 
     void Update()
     {
-        deltaTime += (Time.deltaTime - deltaTime) * 0.1f;
-        float fps = 1.0f / deltaTime;
-        fpsText.text = Mathf.Ceil(fps).ToString();
+        if( m_timeCounter < m_refreshTime )
+        {
+            m_timeCounter += Time.deltaTime;
+            m_frameCounter++;
+        }
+        else
+        {
+            
+            m_lastFramerate = (float)m_frameCounter/m_timeCounter;
+            m_frameCounter = 0;
+            m_timeCounter = 0.0f;
+        }
     }
 }
